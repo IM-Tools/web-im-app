fa8c7f30eb51a2c3d71c35d19511d15539cd28c4
pl1998 <2139714249@qq.com>
2021-07-07 15:37:17 +0800

    vite版本配置调整 自述文件调整

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,22 @@ module.exports = {
     alias: {
         '/@/': path.resolve(__dirname, './src')
     },
-    hostname: '127.0.0.1', // 默认是 localhost
-    port: '3002', // 默认是 3000 端口
-    open: true, // 浏览器自动打开
-    https: false, // 是否开启 https/*  */
-    ssr: false, // 服务端渲染
-    optimizeDeps: {
-        include: ['axios']
+    server: {
+        host: '127.0.0.1',
+        port: 3002,
+        open: true,
+        https: false,
+        proxy: {}
     },
+    
+    // host: '127.0.0.1', // 默认是 localhost
+    // port: '3002', // 默认是 3000 端口
+    // open: true, // 浏览器自动打开
+    // https: false, // 是否开启 https/*  */
+    // ssr: false, // 服务端渲染
+    // optimizeDeps: {
+    //     include: ['axios']
+    // },
     base: './', // 生产环境下的公共路径
     outDir: 'dist', // 打包构建输出路径，默认 dist ，如果路径存在，构建之前会被删除
     proxy: {
