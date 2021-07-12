96a405c88da2a801d8b0e3f9bc519ea625224dd1
pl1998 <2139714249@qq.com>
2021-07-12 11:00:16 +0800

    模糊查询🔍 等待API设计

--- src/components/GoodFriend.vue
+++ src/components/GoodFriend.vue
@@ -1,80 +1,41 @@
 <template>
-    <el-dialog title="创建一个群组" v-model="GoodFriendDialogVisible" width="500px"  @close="$emit('update:GoodFriendDialogVisible', false)">
-     <el-form :model="numberValidateForm" ref="numberValidateForm" label-width="100px" class="demo-ruleForm">
-    <el-input type="age" v-model.number="searchFrom.keyword" autocomplete="off"></el-input>
-</el-form>
-<el-table
-    ref="multipleTable"
-    :data="goodslist"
-    tooltip-effect="dark"
-    style="width: 100%"
-    @selection-change="handleSelectionChange">
-    <el-table-column
-      type="selection"
-      width="55">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="120">
-        <template slot-scope="{row}">
-          <img :src="row.avatar">
-          <span>{{row.name}}</span>
-        </template>
-       <img>
-    </el-table-column>
-  </el-table>
-  <div style="margin-top: 20px">
-    <el-button @click="createGroup()">创建</el-button>
-  </div>
+    <el-dialog title="创建一个群组" v-model="GoodFriendDialogVisible" width="500px" @close="$emit('update:GoodFriendDialogVisible', false)">
+        <el-form :model="numberValidateForm">
+            <el-input ref="numberValidateForm" label-width="100px" class="demo-ruleForm" placeholder="请输入内容" prefix-icon="el-icon-search" v-model="searchFrom.keyword" @keyup.enter="searchGoods" @mouseleave="searchGoods"> </el-input>
+        </el-form>
+        <el-table ref="multipleTable" :data="list" tooltip-effect="dark" style="width: 100%; overflow: auto" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="55"> </el-table-column>
+            <el-table-column prefix-icon="el-icon-search" label="选择好友" width="220">
+                <template class="row scope-row" #default="scope">
+                    <img :src="scope.row.avatar" />
+                    <span>{{ scope.row.name }}</span>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div style="margin-top: 20px">
+            <el-button @click="createGroup()">创建</el-button>
+        </div>
     </el-dialog>
 </template>
 <script>
-import { mapState, mapActions } from 'vuex';
+import { ElNotification } from 'element-plus';
+import { mapState } from 'vuex';
 export default {
     name: 'GoodFriend',
     data() {
         return {
             GoodFriendDialogVisible: false,
-            searchFrom:{
-                keyword:undefined
+            searchFrom: {
+                keyword: undefined,
             },
-             tableData: [{
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-08',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-06',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-07',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }],
-         multipleSelection: []
+            multipleSelection: [],
+            list: [],
         };
     },
-      computed: mapState({
+    computed: mapState({
         users: state => state.auth.users,
         auth: state => state.auth.auth,
-        goodslist: state => state.user.goodslist
+        goodslist: state => state.user.goodslist,
     }),
     props: {
         GoodFriendDialogVisible: {
@@ -83,27 +44,54 @@ export default {
         },
     },
     created() {
-      
+        this.list = this.goodslist;
     },
     methods: {
-        createGroup(){
-
+        searchGoods() {
+            if (this.searchFrom.keyword == '' || this.searchFrom.keyword == undefined) {
+                this.list = this.goodslist;
+                return;
+            }
+            let newList = this.list.filter(value => {
+                if (value.name.search(this.searchFrom.keyword) != -1) {
+                    return value;
+                }
+            });
+            this.list = newList;
         },
-         toggleSelection(rows) {
-        if (rows) {
-          rows.forEach(row => {
-            this.$refs.multipleTable.toggleRowSelection(row);
-          });
-        } else {
-          this.$refs.multipleTable.clearSelection();
-        }
-      },
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },
+        createGroup() {
+         console.log(this.multipleSelection)
+         if(this.multipleSelection.length==0){
+           ElNotification({
+             type:"warning",
+             message:"请选择好友"
+           })
+           return
+         }
+        },
+        toggleSelection(rows) {
+            if (rows) {
+                rows.forEach(row => {
+                    this.$refs.multipleTable.toggleRowSelection(row);
+                });
+            } else {
+                this.$refs.multipleTable.clearSelection();
+            }
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
+        },
         handleFriendDialogClose(done) {
             done();
         },
     },
 };
 </script>
+<style lang="scss" scoped>
+span {
+    margin-left: 5px;
+}
+td {
+    border-bottom: 1px solid #fff !important;
+}
+</style>
